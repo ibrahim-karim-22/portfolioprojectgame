70c218196a293e862d0e01bab6035ed8e4c87dde
Sam Golshan <sam.golshan07@gmail.com>
2024-02-06 23:55:42 -0800

    linked roundTimer.js file and added code for roundTimer

--- js/roundTimer.js
+++ js/roundTimer.js
@@ -1,1 +1,25 @@
+let timer;
+let timeLeft = 60;
+const timerSpan = document.getElementById("timer");
 
+function endGame() {
+    clearInterval(timer);
+    timerSpan.innerHTML = 0;
+}
+
+function updateTimer() {
+    if(timeLeft > 0) {
+        timeLeft = timeLeft - 1;
+        timerSpan.innerHTML = timeLeft;
+    } else {
+        endGame();
+        timerSpan.innerHTML = 60;
+    }
+}
+
+function startGame() {
+    console.log("inside the start function");
+    timer = setInterval(updateTimer, 1000);
+    timeLeft = 60
+    updateTimer();
+}
