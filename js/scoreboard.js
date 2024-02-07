cee6371dd5955467ccfc28ad7205ed58ab1101a8
karim <hema_thechamp@yahoo.com>
2024-02-07 17:00:21 -0500

    scoreboard should be functioning

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,10 +1,36 @@
 const imgElements = document.querySelectorAll('.normalPoints');
-const scoreDisplay = document.querySelector('#score')
+const scoreDisplay = document.querySelector('#score');
+const gameContainer = document.querySelector('.game-container');
+let gameStarted = false;
 
 function incrementScore() {
+    if (!gameStarted) {
+        return;
+    }
     const currentScore = parseInt(scoreDisplay.textContent, 10);
-    scoreDisplay.textContent = currentScore + 1;
+    scoreDisplay.textContent = currentScore + 10;
 }
-imgElements.forEach(img) => {
-    img.addEventListener('click', incrementScore);
+function decreaseScore(event) {
+    if(!gameStarted) {
+        return;
+    }
+    const currentScore = parseInt(scoreDisplay.textContent, 10);
+    if (currentScore > 0) {
+        scoreDisplay.textContent = currentScore - 5;
+    }
+    event.stopPropagation(); //this built in function stops the click form propagating further.
 }
+
+function restartScoreboard() {
+    scoreDisplay.textContent = '0';
+    gameStarted = false;
+}
+function scoreStart () {
+    gameStarted = true;
+}
+
+imgElements.forEach((img) => {img.addEventListener('click', incrementScore);});
+document.querySelector('#endGameBtn').addEventListener('click', restartScoreboard);
+document.querySelector('#startGameBtn').addEventListener('click', scoreStart)
+
+gameContainer.addEventListener('click', decreaseScore);
